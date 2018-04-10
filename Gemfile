313d9403e62b3a8b70afd7e11159d37d835b1062
hassan mccutchen <mccutchen.hassan@gmail.com>
2018-04-10 13:43:19 -0400

    install gems

--- Gemfile
+++ Gemfile
@@ -11,7 +11,10 @@ gem 'rails', '~> 5.1.6'
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3'
 # Use Puma as the app server
+gem 'simple_form'
 gem 'puma', '~> 3.7'
+gem 'webpacker', '~> 3.4'
+
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,47 @@
 class TasksController < ApplicationController
+
+
+
+
+
+
+  def index
+    @tasks = Task.all
+
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+
+  end
+
+  def create
+    @task = Task.create(task_params)
+    if @task.save
+      format.html { redirect_to tasks_path, notice: 'Task was successfully created!.' }
+
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
+    def edit
+    end
+
+    private
+
+    def task_params
+
+      params.require(:task).permit(:title, :details, :completed)
+    end
+
+
+  end
 end
